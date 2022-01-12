4d03212bc552002a699fc95576065e8e204ca1eb
Oleg Iavorskyi <oleg.iavorskyi@gmail.com>
2022-01-12 14:00:50 -0500

    Generate project with start.spring.io

---
+++ src/test/java/com/jugglinhats/hexagonal/storefront/StorefrontApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jugglinhats.hexagonal.storefront;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorefrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
